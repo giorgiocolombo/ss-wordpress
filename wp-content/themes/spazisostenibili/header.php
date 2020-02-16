0b4b3f0f8658fa3f6ab25f51dfe95126a2c91e64
Giorgio <giorgiocolomb@gmail.com>
2020-02-16 12:17:45 +0100

    index

--- wp-content/themes/spazisostenibili/header.php
+++ wp-content/themes/spazisostenibili/header.php
@@ -21,18 +21,11 @@
 			'container_class' => 'menu',
 			'menu_id' => '',
             'menu_class' => '',
-            'before' => '<div class="hover_a_menu"></div>',
 		])?>
-        <!-- <nav class="menu">
-                <a href="<?php echo(SS_URL)?>" class="linkmenu link1"> <div class="hover_a_menu hover1"></div> Home </a>
-                <a href="about" class="linkmenu link2"> <div class="hover_a_menu hover2"></div> About</a>
-                <a href="archivio" class="linkmenu link3"> <div class="hover_a_menu hover3"></div> Archivio</a>
-                <img src="<?php echo SS_INCLUDES ?>img/black_full_logo.svg" alt="logo Spazi Sostenibili" class="logo_menu">
-        </nav> -->
         <a href="<?php echo(SS_URL)?>" class="a_logo"><img src="<?php echo SS_INCLUDES ?>img/logo.svg" alt="Logo" class="logo"><p class="logo_p" >Spazi Sostenibili</p></a>
         <img src="<?php echo SS_INCLUDES ?>img/ricerca.svg" alt="Cerca" class="search">
         <div class="search_form_container">
-            <form action="search.php" class="search_form" id="search_form">
+            <form action="<?php echo(SS_URL)?>?s=" class="search_form" id="search_form">
                 <input type="text" name="search_input">
             </form>
         </div>
--- wp-content/themes/spazisostenibili/index.php
+++ wp-content/themes/spazisostenibili/index.php
@@ -19,154 +19,73 @@
     </section>
     <section class="article_section">   
         <div id="article">
-            <div class="container_article ca_1" data-aos="fade-up" data-aos-duration="600">
-                <div class="text_card">
-                    <div class="tags">
-                        <p>Autore</p>
-
+            <?php $the_query =  new WP_Query( array( 'posts_per_page' => 2, 'post_type' => 'post') ); ?>
+                <?php $containerarticle=1; while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+                    <div class="container_article ca_<?php echo($containerarticle); $containerarticle++?>" data-aos="fade-up" data-aos-duration="600">
+                    <div class="text_card">
+                        <div class="tags">
+                            <p><?php the_author()?></p>
+    
+                        </div>
+                        <h2><?php the_title()?></h2>
+                        <p class="text_card_p"><?php echo(get_the_excerpt())?></p>
+                        <div class="interaction">
+                            <a href="<?php the_permalink() ?>" class="button_article">Continua a leggere</a>
+                        </div>
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-                    <div class="interaction">
-                        <a href="article.php" class="button_article">Continua a leggere</a>
+                    <div class="pic_card_container">
+                        <img class="pic_card" src="<?php echo(get_the_post_thumbnail_url())?>">
                     </div>
-                </div>
-                <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579194044198-cdd94102ef46.jpg">
-                </div>
-                <div class="green_card"></div>
-                <div class="pink_card"></div>
-            </div>
-            <div class="container_article ca_2" data-aos="fade-up" data-aos-duration="600">
-                <div class="text_card">
-                    <div class="tags">
-                        <p>Autore</p>
-
+                    <div class="green_card"></div>
+                    <div class="pink_card"></div>
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-                    <div class="interaction">
-                        <a href="article.php" class="button_article">Continua a leggere</a>
-                    </div>
-                </div>
-                <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg">
-                </div>
-                <div class="green_card"></div>
-                <div class="pink_card"></div>
+                <?php 
+                endwhile;
+                wp_reset_postdata();
+                ?> 
             </div>
     </section>
     <section id="gallery">
         <div class="gallery_posts" data-aos="fade-up" data-aos-duration="600">
-            <a href="gallery.php" class="gallery_item gallery_item_1">
-                <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 1</p>
-                <img src="img-samples/photo-1579126999978-eac334843123.jpg" alt="Gallery 1" class="gallery_img">
-            </a>
-
-            <a href="gallery.php" class="gallery_item gallery_item_2">
-                <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 2</p>
-                <img src="img-samples/photo-1579160855213-a6e60979eecf.jpg" alt="Gallery 2" class="gallery_img">
-            </a>
-
-            <a href="gallery.php" class="gallery_item gallery_item_3">
-                <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 3</p>
-                <img src="img-samples/photo-1579189880841-43ddba8d8efd.jpg" alt="Gallery 3" class="gallery_img">
-            </a>
-
-            <a href="gallery.php" class="gallery_item gallery_item_4">
-                <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 4</p>
-                <img src="img-samples/photo-1579191748111-90d7f89fe214 (1).jpg" alt="Gallery 4" class="gallery_img">
-            </a>
-
-            <a href="gallery.php" class="gallery_item gallery_item_5">
-                <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 5</p>
-                <img src="img-samples/photo-1579194044198-cdd94102ef46.jpg" alt="Gallery 5" class="gallery_img">
-            </a>
-
-            <a href="gallery.php" class="gallery_item gallery_item_6">
-                <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 6</p>
-                <img src="img-samples/photo-1579164618222-dfb28558a476.jpg" alt="Gallery 5" class="gallery_img">
-            </a>
-            
+        <?php $the_query =  new WP_Query( array( 'posts_per_page' => 6, 'post_type' => 'gallerie') ); ?>
+                <?php $galleryitem=1; while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+                <a href="<?php the_permalink() ?>" class="gallery_item gallery_item_<?php echo($galleryitem); $galleryitem++?>">
+                    <div class="gallery_cover"></div>
+                    <p class="figcaption"><?php the_title()?></p>
+                    <img src="<?php echo(get_the_post_thumbnail_url())?>" alt="Gallery 1" class="gallery_img">
+                </a>
+                <?php 
+                endwhile;
+                wp_reset_postdata();
+                ?>      
         </div>
     </section>
     <section class="article_section">   
         <div id="article">
-            <div class="container_article ca_3" data-aos="fade-up" data-aos-duration="600">
-                <div class="text_card">
-                    <div class="tags">
-                        <p>Autore</p>
-
+        <?php $the_query =  new WP_Query( array( 'posts_per_page' => 4, 'post_type' => 'post', 'offset' => 2) ); ?>
+                <?php $containerarticle=3; while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+                    <div class="container_article ca_<?php echo($containerarticle); $containerarticle++?>" data-aos="fade-up" data-aos-duration="600">
+                    <div class="text_card">
+                        <div class="tags">
+                            <p><?php the_author()?></p>
+    
+                        </div>
+                        <h2><?php the_title()?></h2>
+                        <p class="text_card_p"><?php echo(get_the_excerpt())?></p>
+                        <div class="interaction">
+                            <a href="<?php the_permalink() ?>" class="button_article">Continua a leggere</a>
+                        </div>
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-                    <div class="interaction">
-                        <a href="article.php" class="button_article">Continua a leggere</a>
+                    <div class="pic_card_container">
+                        <img class="pic_card" src="<?php echo(get_the_post_thumbnail_url())?>">
                     </div>
-                </div>
-                <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579194044198-cdd94102ef46.jpg">
-                </div>
-                <div class="green_card"></div>
-                <div class="pink_card"></div>
-            </div>
-            <div class="container_article ca_4" data-aos="fade-up" data-aos-duration="600">
-                <div class="text_card">
-                    <div class="tags">
-                        <p>Autore</p>
+                    <div class="green_card"></div>
+                    <div class="pink_card"></div>
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-                    <div class="interaction">
-                        <a href="article.php" class="button_article">Continua a leggere</a>
-                    </div>
-                </div>
-                <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg">
-                </div>
-                <div class="green_card"></div>
-                <div class="pink_card"></div>
-            </div>
-            <div class="container_article ca_5" data-aos="fade-up" data-aos-duration="600">
-                <div class="text_card">
-                    <div class="tags">
-                        <p>Autore</p>
-                    </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-                    <div class="interaction">
-                        <a href="article.php" class="button_article">Continua a leggere</a>
-                    </div>
-                </div>
-                <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg">
-                </div>
-                <div class="green_card"></div>
-                <div class="pink_card"></div>
-            </div>
-            <div class="container_article ca_6" data-aos="fade-up" data-aos-duration="600">
-                <div class="text_card">
-                    <div class="tags">
-                        <p>Autore</p>
-                    </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-                    <div class="interaction">
-                        <a href="article.php" class="button_article">Continua a leggere</a>
-                    </div>
-                </div>
-                <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg">
-                </div>
-                <div class="green_card"></div>
-                <div class="pink_card"></div>
-            </div>
+                <?php 
+                endwhile;
+                wp_reset_postdata();
+                ?> 
     </section>
     <div class="link_to_archive_div">
         <a href="<?php echo(SS_URL)?>archivio" class="link_to_archive" data-aos="zoom-in" data-aos-duration="150" data-aos-delay="100">Vai all'archivio</a>
